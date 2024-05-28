eb393a40f7ef751b88ffc97718d74e3b2865f5fb
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-05-28 03:04:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+var list = document.getElementById("list")
+
+
+function addTodo(){
+    var todo_item = document.getElementById("todo-item")
+    var li = document.createElement("li")
+
+    console.log(li)
+}
