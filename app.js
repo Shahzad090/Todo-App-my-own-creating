f5fc69c2a7237c8e229cefec0c7787a84c9cdb9c
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-05-28 03:16:30 +0100

    continue

--- app.js
+++ app.js
@@ -5,5 +5,9 @@ function addTodo(){
     var todo_item = document.getElementById("todo-item")
     var li = document.createElement("li")
 
+var liText = document.createTextNode("shahzad")
+
+li.appendChild(liText)
+
     console.log(li)
 }
